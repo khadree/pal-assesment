2061793baf47760e8468bda8d77c983b59a5b049
khadreeg <khadreegeorge@gmail.com>
2026-03-10 22:32:53 +0100

    test

---
+++ app/app.test.js
@@ -0,0 +1,15 @@
+const request = require("supertest");
+const app = require("../src/app");
+
+describe("Health endpoint", () => {
+
+  it("should return healthy", async () => {
+
+    const res = await request(app).get("/health");
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.status).toBe("healthy");
+
+  });
+
+});
